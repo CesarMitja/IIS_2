cb0c32a0eb190244dae6568819bf06d67e97e7fb
CesarMitja <mitja.cesar1@student.um.si>
2024-04-22 13:44:02 +0200

    Testiranje delovanja pred zagovorom

--- pytest/test_2.py
+++ pytest/test_2.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 import os
 from flask import json
 import sys
@@ -20,3 +21,19 @@ def test_predict_failure(client):
     response = client.post('/predict', content_type='application/json')
     assert response.status_code == 200
     data = json.loads(response.data)
+
+def test_mbajk_api():
+    API_URL = "https://api.jcdecaux.com/vls/v1/stations?contract=maribor&apiKey=5e150537116dbc1786ce5bec6975a8603286526b"
+    response = requests.get(API_URL)
+    assert response.status_code == 200
+
+def test_mateo_api():
+    WEATHER_API_URL = "https://api.open-meteo.com/v1/forecast"
+    WEATHER_PARAMS = {
+    'latitude': 46.5547,
+    'longitude': 15.6467,
+    'hourly': 'temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,rain,surface_pressure'
+    }
+    response = requests.get(WEATHER_API_URL, params=WEATHER_PARAMS)
+    assert response.status_code == 200
+    
--- src/data/Process_data.py
+++ src/data/Process_data.py
@@ -1,16 +1,14 @@
 import pandas as pd
 from pathlib import Path
 
-# Paths for data files
+
 BIKE_DATA_FILE = Path("data/raw/kolesa.csv")
 WEATHER_DATA_FILE = Path("data/raw/vreme.csv")
 OUTPUT_DATA_FILE = Path("data/processed/data_for_prediction.csv")
 
-# Load datasets
 bike_data = pd.read_csv(BIKE_DATA_FILE, parse_dates=['last_update'])
 weather_data = pd.read_csv(WEATHER_DATA_FILE, parse_dates=['time'])
 
-# Refine weather data: rename columns for clarity and select necessary ones
 weather_columns_rename = {
     'temperature_2m': 'temperature',
     'relative_humidity_2m': 'relative_humidity',
@@ -23,11 +21,9 @@ weather_columns_rename = {
 weather_data.rename(columns=weather_columns_rename, inplace=True)
 weather_data = weather_data[['time', 'temperature', 'relative_humidity', 'dew_point', 'apparent_temperature', 'precipitation_probability', 'rain', 'surface_pressure']]
 
-# Align time columns in both datasets to the nearest hour
 bike_data['last_update'] = bike_data['last_update'].dt.round('H')
 weather_data['time'] = weather_data['time'].dt.round('H')
 
-# Merge datasets on time column
 combined_data = pd.merge_asof(
     bike_data.sort_values('last_update'), 
     weather_data.sort_values('time'), 
@@ -38,16 +34,11 @@ combined_data = pd.merge_asof(
 
 combined_data.rename(columns={'last_update': 'date'}, inplace=True)
 
-# Prepare the final DataFrame
 combined_data = combined_data[['date','temperature', 'relative_humidity', 'dew_point', 'apparent_temperature',
             'precipitation_probability', 'rain', 'surface_pressure', 'bike_stands', 'available_bike_stands']]
 
-
-
-# Set timestamp to UTC timezone
 combined_data['date'] = combined_data['date'].dt.tz_localize('UTC')
 
-# Save the combined data
 combined_data.to_csv(OUTPUT_DATA_FILE, index=False)
 
 print(f"Data saved to {OUTPUT_DATA_FILE}.")
